7bf8af2d78a0f0528144d0aa6980e96d8819e888
Igor Cunha <igorcesarcunha@gmail.com>
2021-03-14 20:14:02 -0300

    challenge: Second Rocketseat Challenge"

--- src/index.js
+++ src/index.js
@@ -10,19 +10,62 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const user = users.find(user => user.username === username);
+
+  request.user = user;
+
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  if ((user.pro === false && user.todos.length >= 10)) {
+    return response.status(403);
+  }
+
+  return next();
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const user = users.find(user => user.username === username);
+  if (!user) {
+    return response.status(404);
+  }
+
+  const { id } = request.params;
+  const validId = validate(id);
+  if (!validId) {
+    return response.status(400);
+  }
+
+  
+
+  const todo = user.todos.find(todo => todo.id === id);
+
+
+  if (!todo) {
+    return response.status(404);
+  }
+
+  request.todo = todo;
+  request.user = user;
+
+  return next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+  const user = users.find(user => user.id === id);
+
+  if (!user) {
+    return response.status(404)
+  }
+
+  request.user = user;
+  return next();
 }
 
 app.post('/users', (request, response) => {
